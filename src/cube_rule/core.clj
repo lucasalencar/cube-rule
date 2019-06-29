4e6875d08de3db272846e7dbfc6b4e5be1104bc6
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-06-29 12:48:57 -0300

    Add facts to check food categories

--- src/cube_rule/core.clj
+++ src/cube_rule/core.clj
@@ -16,8 +16,55 @@
    [eatable :cat-food :cat]
    [eatable :fish :cat]))
 
-(pldb/with-db eatable-facts
-  (logic/run* [eatable-thing]
-    (logic/fresh [living-being]
-      (logic/== living-being :human)
-      (eatable eatable-thing living-being))))
+(pldb/db-rel toast food)
+(pldb/db-rel sandwich food)
+(pldb/db-rel taco food)
+(pldb/db-rel sushi food)
+(pldb/db-rel quiche food)
+(pldb/db-rel calzone food)
+(pldb/db-rel salad food)
+
+(def cube-rule-facts
+  (pldb/db
+   [toast :pizza]
+   [toast :nigiri-sushi]
+   [toast :pumpkin-pie-slice]
+
+   [sandwich :lasagna]
+   [sandwich :toast]
+   [sandwich :quesadilla]
+
+   [taco :hot-dog]
+   [taco :sub-sandwich]
+   [taco :slice-of-pie]
+
+   [sushi :falafel-wrap]
+   [sushi :pigs-in-a-blanket]
+   [sushi :enchilada]
+
+   [quiche :cheesecake]
+   [quiche :soup]
+   [quiche :falafel-pita]
+   [quiche :deep-dish-pizza]
+   [quiche :salad]
+   [quiche :key-lime-pie]
+
+
+   [calzone :burrito]
+   [calzone :corn-dog]
+   [calzone :pie]
+   [calzone :dumplings]
+   [calzone :pop-tarts]
+   [calzone :uncrustables]
+
+
+   [salad :steak]
+   [salad :mashed-potatoes]
+   [salad :fried-rice]
+   [salad :spaghetti]
+   [salad :poutine]
+   [salad :soup]))
+
+(pldb/with-db cube-rule-facts
+  (logic/run* [category food]
+    (toast food)))
