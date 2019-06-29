4477e5f75bd4c39dfdf4127885d5c56449f2a015
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-06-29 12:28:33 -0300

    Add facts experiment to find out eatable things

--- src/cube_rule/core.clj
+++ src/cube_rule/core.clj
@@ -1,1 +1,23 @@
-(ns cube-rule.core)
+(ns cube-rule.core
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic :as logic]
+            [clojure.core.logic.pldb :as pldb]))
+
+(pldb/db-rel eatable eatable-thing living-being)
+
+(def eatable-facts
+  (pldb/db
+   [eatable :burrito :human]
+   [eatable :pizza :human]
+   [eatable :nigiri-sushi :human]
+
+   [eatable :dog-food :dog]
+
+   [eatable :cat-food :cat]
+   [eatable :fish :cat]))
+
+(pldb/with-db eatable-facts
+  (logic/run* [eatable-thing]
+    (logic/fresh [living-being]
+      (logic/== living-being :human)
+      (eatable eatable-thing living-being))))
