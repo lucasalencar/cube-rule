a7aa2efaca3c65ef858c775c3372806730ac3f8b
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2019-06-29 11:07:51 -0300

    Create cube-rule project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cube-rule
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cube_rule/core_test.clj
@@ -0,0 +1,7 @@
+(ns cube-rule.core-test
+  (:require [clojure.test :refer :all]
+            [cube-rule.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
